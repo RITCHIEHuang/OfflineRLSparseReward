c4b08af33193ce30edd850bf865c8b7bf396d9d5
RITCHIEHuang <ritchie445412@gmail.com>
2021-11-30 00:35:57 +0800

    refactor

--- datasets/qlearning_dataset.py
+++ datasets/qlearning_dataset.py
@@ -37,19 +37,18 @@ def qlearning_dataset(env, dataset=None, terminate_on_end=False, **kwargs):
 
     # The newer version of the dataset adds an explicit
     # timeouts field. Keep old method for backwards compatability.
-    last_done_step = None
     use_timeouts = False
     if "timeouts" in dataset:
         use_timeouts = True
 
     episode_step = 0
-    for i in range(N - 1):
+    i = 0
+    while i < (N - 1):
         obs = dataset["observations"][i].astype(np.float32)
         new_obs = dataset["observations"][i + 1].astype(np.float32)
         action = dataset["actions"][i].astype(np.float32)
         reward = dataset["rewards"][i].astype(np.float32)
         done_bool = bool(dataset["terminals"][i])
-        done_bool_next = bool(dataset["terminals"][i + 1])
         time_out = bool(dataset["timeouts"][i])
 
         if use_timeouts:
@@ -60,11 +59,8 @@ def qlearning_dataset(env, dataset=None, terminate_on_end=False, **kwargs):
             # Skip this transition and don't apply terminals on the last step of an episode
             episode_step = 0
             continue
-        if done_bool and not done_bool_next or final_timestep:
+        if done_bool or final_timestep:
             episode_step = 0
-
-        if done_bool and done_bool_next:
-            done_bool = not done_bool
 
         obs_.append(obs)
         next_obs_.append(new_obs)
@@ -73,6 +69,12 @@ def qlearning_dataset(env, dataset=None, terminate_on_end=False, **kwargs):
         done_.append(done_bool)
         time_out_.append(time_out)
         episode_step += 1
+
+        # truncate useless data
+        if done_bool:
+            while bool(dataset["terminals"][i + 1]):
+                i += 1
+        i += 1
 
     return {
         "observations": np.array(obs_),
